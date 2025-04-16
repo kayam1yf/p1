26652d09ed47565f53af5c6cece1db0ac8158b89
kayam1yf <kayam1yf@gmail.com>
2025-04-16 11:01:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
+script.js
+document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessiblidade')
 
-    botaoDeAcessibilidade.addEventListener('click', function () {
+    botaoDeAcessibilidade.addEventListener('click', function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-
     })
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
@@ -13,15 +13,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let tamanhoAtualFonte = 1;
 
-    aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte += 0.1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte +=0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
+        
     })
 
-    diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte -= 0.1;
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -=0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
+        
     })
+
+
 })
